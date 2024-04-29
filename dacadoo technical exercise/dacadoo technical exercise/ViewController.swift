57c62e417b41617f10e4ad73343cceba19024cad
Santamarian <santamarian.bogdan@gmail.com>
2024-04-29 11:54:26 +0300

    create xcode project

---
+++ dacadoo technical exercise/dacadoo technical exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dacadoo technical exercise
+//
+//  Created by Santamarian Bogdan on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
